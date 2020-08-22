070746cd4e106d659485065f9e8b156df11791a1
alex07L <alexlevesque7@hotmail.fr>
2020-08-21 23:34:17 -0400

    Update streamer.go

--- streamer.go
+++ streamer.go
@@ -13,16 +13,21 @@ func main() {
 	fmt.Println(getContent("07112"))
 }
 
+//convert data to html
 func getContent(zipcode string) string {
 
 	var result map[string]interface{}
-	json.Unmarshal([]byte(call(zipcode)), &result)
+	//convert strign to a map of string (json) and put result on resulat variable
+	json.Unmarshal(call(zipcode), &result)
+	//convert json to a map of string and convert flaot to a string
 	return "<div>" + strconv.FormatFloat(result["current"].(map[string]interface{})["temp_c"].(float64), 'f', 2, 64) + "</div>"
 }
 
+//call function to get a meteo data
 func call(zipcode string) []byte {
 	resp, _ := http.Get("http://api.weatherapi.com/v1/forecast.json?key=2b23e7071c304d53b82160733203007&q=" + zipcode + "&day=1")
 	body, _ := ioutil.ReadAll(resp.Body)
+	//close connection
 	resp.Body.Close()
 	return body
 }
